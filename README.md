1e976b1a2ca0916184cc260de062e6a6d3553e10
EmmaHammar <emma.s.hammar@gmail.com>
2021-09-23 09:39:57 +0200

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
+# main.js skapar vår app - vi ändrar ej där
+# app.vue är vår ingång - här börjar vårt komponentträd
+# public -> index.html: renderar innehållet från <div id="app"></div> - här ändrar vi ej heller
+
+Komponent-scopat
+Allt stannar i komponentent - både js o css
+
+# starta utvecklingsserver :
+npm run serve -> klistra in adressen i webbläsare
+
+# städa
+- ta bort komponent i components-mappen samt rensa bort komponenten + img i App.vue
+- glöm inte ladda om sidan efter du ändrat något för att se så allt funkar
+
+# visar
+- fånga klick
+- if-staser
+- fånga formulär
+
 # vue-intro
 
 ## Project setup
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello VUE</h1>
+    <!-- <p>Välkommen {{userName}}</p> -->
+    <p>Välkommen {{ user.firstName }} {{ user.lastName }}</p>
+
+    <!-- räknar-exemplet: -->
+    <div>
+      Du har klickat {{ clicked }} ggr
+      <button @click="onClick">Klicka</button>
+    
+
+      <!-- vill visa nedan div när vi klickat 5 ggr. v- ser Vues inbyggda alternativ. Vue använder inte hide utan Bra-klickat existerar inte i DOMen förrän v-if har blivit true. -->
+      <div v-if="clicked > 5">
+        Bra klickat!
+      </div>
+
+    
+      <!-- förifyllda värden: v-model="user.firstName" -->
+      <form @submit="handleSubmit">
+        Förnamn: <input type="text" v-model="user.firstName" id="firstName"> <br />
+        Efternamn: <input type="text" v-model="user.lastName" id="lastName"> <br />
+        <button>Spara</button>
+      </form>
+      
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  //detta kan vara strängar, tal, listor, booleans - alla typer som JS kan hantera
+  data() {
+    return {
+      // userName: "Janne",
+      // user: {firstName: "Janne", lastName: "Kemi"},
+      user: {firstName: localStorage.getItem("firstName"), lastName: localStorage.getItem("lastName")},
+      clicked: 0,
+    }
+  }, 
+
+  methods: {
+    onClick() {
+      console.log("klick på knapp");
+      this.clicked ++
+    },
+    handleSubmit(event) {
+      event.preventDefault();
+
+      //fånga värdet
+      console.log("formulär sparat", event.target.firstName.value);
+
+      //spara i lS. //stringify för att spara, parse för att hämta. men i ex gör 2 olika:
+      localStorage.setItem("firstName", event.target.firstName.value)
+      localStorage.setItem("lastName", event.target.lastName.value)
+
+    }
+
   }
+
 }
+
+//skapa en data
+//allt som vi skriver i script måste vi exportera som defaults. Om vi har data som vi har tillgång till o vill visa i komponenten måste det finnas innanför export default
 </script>
 
 <style>
